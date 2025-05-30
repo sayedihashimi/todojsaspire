ee40d06b690c4c0bb7ea0fdb6273f08ee8597bec
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-05-30 11:39:30 -0400

    adding model class

---
+++ src/TodojsAspire.ApiService/Todo.cs
@@ -0,0 +1,13 @@
+namespace TodojsAspire.ApiService;
+
+public class Todo
+{
+    public int Id{get;set;}
+    [Required]
+    public string Title{get;set;}=default!;
+    public bool IsComplete{get;set;}=false;
+    // The position of the todo in the list, used for ordering.
+    // When updating this value, make sure to not duplicate values.
+    // To move an item up/down it's best to swap the values of the position
+    public int Position{get;set;}=0;
+}
