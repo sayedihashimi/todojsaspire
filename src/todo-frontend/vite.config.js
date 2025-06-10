d228efe440ade10957b2da4770c309011c502d74
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-06-10 12:06:52 -0400

    working on AddViteApp, needs update

--- src/todo-frontend/vite.config.js
+++ src/todo-frontend/vite.config.js
@@ -1,27 +1,28 @@
-import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     server:{
-      port: parseInt(env.VITE_PORT),
       proxy: {
         // "apiservice" is the name of the API in AppHost.cs.
         '/api': {
-          target: process.env.services__apiservice__https__0 || process.env.services__apiservice__http__0,
+          target: 
+            process.env.services__apiservice__https__0 || 
+            process.env.services__apiservice__http__0,
           changeOrigin: true,
           secure: false,
-          rewrite: (path) => path.replace(/^\/api/, '')
+          rewrite: (path) => path.replace(/^\/api/, "")
         }
       }
     },
     build:{
-      outDir: 'dist',
+      outDir: "dist",
       rollupOptions: {
-        input: '.index.html'
+        input: ".index.html"
       }
     }
   }
